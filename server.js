232811dd7dc5607884ef473a2770be1e6a0faf67
yashi131tak <yashitak131@gmail.com>
2017-02-05 21:50:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,18 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('article-one',function(req,res)
+{
+    res.send('article one is requested and will be served here');
+});
+app.get('article-two',function(req,res)
+{
+    res.send('article two is requested and will be served here');
+});
+app.get('article-three',function(req,res)
+{
+    res.send('article three is requested and will be served here');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
