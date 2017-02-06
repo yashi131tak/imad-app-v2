2c3a710ede081ce942486f9e0f8e803ade66a6ad
yashi131tak <yashitak131@gmail.com>
2017-02-06 18:44:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ app.get('/article-three',function(req,res)
 {
     res.send('article three is requested and will be served here');
 });
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
